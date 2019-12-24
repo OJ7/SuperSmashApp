da228a85f6c747b81964cba172c2aea18d832814
Omair Javed <omairj7@gmail.com>
2019-12-24 18:26:38 -0500

    Adding character model

---
+++ src/app/models/character.model.ts
@@ -0,0 +1,10 @@
+
+export class Character {
+
+    public name: string;
+    public attackPower: number;
+    public defensePower: number;
+    public percentage: number;
+    public numStocks: number;
+
+}
